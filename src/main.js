9dbaf18346b9b23d7f6a443da5d2d457f5a43580
Ansa3526 <shahepesar40@gmail.com>
2024-07-26 11:51:51 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from "./router";
 import Particles from "particles.vue";
-
+const someChanges = 1;
 import VueFinalModal from 'vue-final-modal';
 /*eslint-disable */
 // load global script
